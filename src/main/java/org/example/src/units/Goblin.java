c7a6cfb7c2e5bb7f0a3be6599c908d87f32f1a7e
SuraSurow <surowiecki.wojciech1@gmail.com>
2024-02-29 20:24:00 +0100

    Add save and load option

--- src/main/java/org/example/src/units/Goblin.java
+++ src/main/java/org/example/src/units/Goblin.java
@@ -1,8 +1,14 @@
 package org.example.src.units;
 
+
+import java.util.List;
+
 public class Goblin extends Unit {
     public Goblin()
     {
-        super("Goblin",30,15);
+        super("Goblin",30,15,30);
+    }
+    public Goblin(List<String> list){
+        super(list);
     }
 }
--- src/main/java/org/example/src/units/Hero.java
+++ src/main/java/org/example/src/units/Hero.java
@@ -1,12 +1,30 @@
 package org.example.src.units;
 
-public class Hero extends Unit{
-    protected static int maxHeal = 5;
-    protected static int currentLevel = 1;
-    public Hero (String name){
-        super(name,100,10);
-    }
 
+import java.util.List;
+
+
+public class Hero extends Unit{
+    protected static int maxHeal;
+    protected static int currentLevel;
+    public Hero (String name){
+        super(name,100,10,100);
+        maxHeal = 5;
+        currentLevel = 1;
+    }
+    public Hero(List<String> list) {
+        super(
+                list.get(0),
+                Integer.parseInt(list.get(1)),
+                Integer.parseInt(list.get(2)),
+                Integer.parseInt(list.get(3))
+        );
+        if (list.size() != 6) {
+            throw new IllegalArgumentException("List must have 6 argument");
+        }
+        maxHeal = Integer.parseInt(list.get(4));
+        currentLevel = Integer.parseInt(list.get(5));
+    }
     @Override
     public void showStats(){
         System.out.println(
@@ -18,6 +36,18 @@ public class Hero extends Unit{
                         +"\tLevel:"+currentLevel
         );
 
+    }
+    @Override
+    public String toCSV(){
+        String[] attributes = {
+                name,
+                String.valueOf(currentHealth),
+                String.valueOf(maxDamage),
+                String.valueOf(maxHealth),
+                String.valueOf(maxHeal),
+                String.valueOf(currentLevel)
+        };
+        return String.join(",",attributes);
     }
 
     public void levelUp(){
@@ -40,7 +70,7 @@ public class Hero extends Unit{
     public void showAction()
     {
         System.out.println(
-                "1.Attack\t2.Heal\t3.Give Up"
+                "1.Attack\t2.Heal\t3.Give Up\t4.Save Game"
         );
     }
 
